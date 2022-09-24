acf3b5d78ebe83ef9b3393bc8ebaa4f37308dcb6
Yuchen Jiang <lighght@umich.edu>
2022-09-24 16:41:59 -0400

    style: name compute fields

--- compiler/src/syntax.rs
+++ compiler/src/syntax.rs
@@ -19,11 +19,28 @@ pub enum Value<Ann> {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Compute<Ann> {
-    Let(String, Box<Value<Ann>>, Box<Compute<Ann>>, Ann),
-    Do(String, Box<Compute<Ann>>, Box<Compute<Ann>>, Ann),
+    Let {
+        binding: (String, Box<Value<Ann>>),
+        body: Box<Compute<Ann>>,
+        ann: Ann,
+    },
+    Do {
+        binding: (String, Box<Compute<Ann>>),
+        body: Box<Compute<Ann>>,
+        ann: Ann,
+    },
     Force(Box<Value<Ann>>, Ann),
     Return(Box<Value<Ann>>, Ann),
-    Lam(String, Box<TValue<Ann>>, Box<Compute<Ann>>, Ann),
+    Lam {
+        arg: (String, Box<TValue<Ann>>),
+        body: Box<Compute<Ann>>,
+        ann: Ann,
+    },
     App(Box<Compute<Ann>>, Box<Value<Ann>>, Ann),
-    If(Box<Value<Ann>>, Box<Compute<Ann>>, Box<Compute<Ann>>, Ann),
+    If {
+        cond: Box<Value<Ann>>,
+        thn: Box<Compute<Ann>>,
+        els: Box<Compute<Ann>>,
+        ann: Ann,
+    },
 }
