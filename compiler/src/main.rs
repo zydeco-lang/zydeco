e87b110255138fd219674e8243ead25d2d9c5707
Yuchen Jiang <lighght@umich.edu>
2022-09-25 02:26:12 -0400

    test: add cases

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -2,8 +2,22 @@ use vituloid_compiler::parser::ComputationParser;
 
 fn main() {
     let stdin = std::io::stdin();
+    let mut buffer = String::new();
+    const MARKER: &str = "@@@";
     for line in stdin.lines() {
         let line = line.unwrap();
-        println!("{:?}", ComputationParser::new().parse(&line).unwrap());
+        if line.starts_with(MARKER) {
+            let title = line
+                .trim_start_matches(MARKER)
+                .trim_end_matches(MARKER)
+                .trim();
+            println!(">>> [{}]", title);
+            println!("{:?}", ComputationParser::new().parse(&buffer).unwrap());
+            println!("<<< [{}]", title);
+            println!();
+            buffer.clear()
+        } else {
+            buffer.push_str(&line);
+        }
     }
 }
