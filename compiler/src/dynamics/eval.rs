911523f05eb1ba490d2b2309ae7add91cece8544
Runze Xue <cactus@umich.edu>
2022-09-24 23:25:24 -0400

    style(eval): reformat duplicate codes

--- compiler/src/dynamics/eval.rs
+++ compiler/src/dynamics/eval.rs
@@ -3,24 +3,27 @@ use std::collections::HashMap;
 
 type Env<Ann> = HashMap<String, Vec<Value<Ann>>>;
 
+fn with_eval<Ann: Clone>(
+    env: &mut Env<Ann>, name: &String, val: Value<Ann>, exp: Compute<Ann>,
+) -> Option<Value<Ann>> {
+    env.entry(name.clone()).or_default().push(val);
+    let res = eval_env(env, exp);
+    env.get_mut(name).unwrap().pop();
+    res
+}
+
 fn eval_env<Ann: Clone>(env: &mut Env<Ann>, exp: Compute<Ann>) -> Option<Value<Ann>> {
     use Compute::*;
     use Value::*;
     match exp {
         Let { binding, body, .. } => {
             let (name, val) = binding;
-            env.entry(name.clone()).or_default().push(*val);
-            let result = eval_env(env, *body);
-            env.get_mut(&name).unwrap().pop();
-            result
+            with_eval(env, &name, *val, *body)
         }
         Do { binding, body, .. } => {
             let (name, compute) = binding;
             let val = eval_env(env, *compute)?;
-            env.entry(name.clone()).or_default().push(val);
-            let result = eval_env(env, *body);
-            env.get_mut(&name).unwrap().pop();
-            result
+            with_eval(env, &name, val, *body)
         }
         Force(val, _) => {
             if let Value::Thunk(compute, _) = *val {
@@ -38,19 +41,15 @@ fn eval_env<Ann: Clone>(env: &mut Env<Ann>, exp: Compute<Ann>) -> Option<Value<A
             }),
             ann,
         )),
-        App(f, arg, _ann) => {
-            let f = eval_env(env, *f)?;
-            if let Value::Thunk(compute, _ann) = f {
+        App(f, arg, _) => {
+            if let Value::Thunk(compute, _) = eval_env(env, *f)? {
                 if let Compute::Lam {
                     arg: (name, _),
                     body,
                     ..
                 } = *compute
                 {
-                    env.entry(name.clone()).or_default().push(*arg);
-                    let result = eval_env(env, *body);
-                    env.get_mut(&name).unwrap().pop();
-                    result
+                    with_eval(env, &name, *arg, *body)
                 } else {
                     None
                 }
@@ -58,11 +57,13 @@ fn eval_env<Ann: Clone>(env: &mut Env<Ann>, exp: Compute<Ann>) -> Option<Value<A
                 None
             }
         }
-        If { cond, thn, els, .. } => match *cond {
-            Bool(true, _) => eval_env(env, *thn),
-            Bool(false, _) => eval_env(env, *els),
-            _ => None,
-        },
+        If { cond, thn, els, .. } => {
+            if let Bool(b, _) = *cond {
+                eval_env(env, if b { *thn } else { *els })
+            } else {
+                None
+            }
+        }
     }
 }
 
