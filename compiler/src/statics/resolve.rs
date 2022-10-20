f65a87312ef6f05daf465aa1a157fc4c60db4ec0
Yuchen Jiang <lighght@umich.edu>
2022-10-19 23:03:10 -0400

    feat: add drafts for name resolution

--- compiler/src/statics/resolve.rs
+++ compiler/src/statics/resolve.rs
@@ -1,11 +1,21 @@
+use std::collections::HashMap;
+
 #[derive(Clone, Debug)]
 pub enum NameResolveError<Ann> {
-    DuplicateDeclaration {
-        name: String,
-        ann: Ann,
-    },
-    UnknownIdentifier {
-        name: String,
-        ann: Ann,
-    },
+    DuplicateDeclaration { name: String, ann: Ann },
+    UnknownIdentifier { name: String, ann: Ann },
 }
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum NamespaceSort {
+    Type,
+    Value,
+    Ctor,
+    Dtor,
+}
+
+pub struct NamespaceResolver {
+    pub namespaces: HashMap<(NamespaceSort, String), usize>,
+}
+
+
