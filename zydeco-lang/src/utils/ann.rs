f0cb331c62d6622641a97b29aa5a1013a848b567
Yuchen Jiang <lighght@umich.edu>
2022-11-10 00:22:25 -0500

    chore: remove stupid

--- zydeco-lang/src/utils/ann.rs
+++ zydeco-lang/src/utils/ann.rs
@@ -16,17 +16,3 @@ pub trait AnnHolder<Ann: AnnT> {
     fn ann(&self) -> &Ann;
     fn set_ann(&mut self, ann: Ann);
 }
-
-pub trait AnnMap<Ann1: AnnT, Ann2: AnnT> {
-    type Output<Ann: AnnT>;
-    fn map_ann<F: Fn(&Ann1) -> Ann2>(&self, f: F) -> Self::Output<Ann2>;
-}
-
-impl<Ann: AnnT, T: AnnHolder<Ann> + Clone> AnnMap<Ann, Ann> for T {
-    type Output<AnnS: AnnT> = Self;
-    fn map_ann<F: Fn(&Ann) -> Ann>(&self, f: F) -> Self::Output<Ann> {
-        let mut t = self.clone();
-        t.set_ann(f(self.ann()));
-        t
-    }
-}
