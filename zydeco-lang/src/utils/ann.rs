e5620b92da91770d83354742e374e246baf2f299
Yuchen Jiang <lighght@umich.edu>
2022-12-07 08:06:22 -0500

    [ann] add span1 to ann

--- zydeco-lang/src/utils/ann.rs
+++ zydeco-lang/src/utils/ann.rs
@@ -1,10 +1,12 @@
 use std::fmt::Debug;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Ann {}
+pub struct Ann {
+    span1: (usize, usize),
+}
 
-pub fn ann() -> Ann {
-    Ann {}
+pub fn ann(l: usize, r: usize) -> Ann {
+    Ann { span1: (l, r) }
 }
 
 pub trait AnnHolder {
