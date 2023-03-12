02ad06c8d00a8916d3a7684ec86c0e87e9cd4546
LighghtEeloo <litiaeeloo@gmail.com>
2023-03-11 23:34:57 -0500

    [derive] support both named and unnamed, single and multiple fields

--- zydeco-lang/derive/src/lib.rs
+++ zydeco-lang/derive/src/lib.rs
@@ -11,19 +11,83 @@ pub fn enum_generator_derive(input: TokenStream) -> TokenStream {
         Data::Enum(data) => {
             for variant in &data.variants {
                 let variant_ident = &variant.ident;
-                let field = match &variant.fields {
-                    Fields::Unnamed(field) => field.unnamed.first().expect("EnumGenerator can only be derived for enum variants with one field"),
-                    _ => unreachable!("EnumGenerator can only be derived for enum variants with one field"),
-                };
-                let dom = &field.ty;
-                let gen = quote! {
-                    impl From<#dom> for #cod {
-                        fn from(item: #dom) -> Self {
-                            #cod::#variant_ident(item)
+                match &variant.fields {
+                    Fields::Unnamed(field) => {
+                        if field.unnamed.len() == 1 {
+                            let dom = &field.unnamed.first().expect("EnumGenerator can only be derived for enum variants with one field").ty;
+                            let gen = quote! {
+                                impl From<#dom> for #cod {
+                                    fn from(item: #dom) -> Self {
+                                        #cod::#variant_ident(item)
+                                    }
+                                }
+                            };
+                            res.extend(TokenStream::from(gen));
+                        } else {
+                            let dom: Vec<_> = (&field.unnamed)
+                                .into_iter()
+                                .map(|f| &f.ty)
+                                .collect();
+                            let idx: Vec<_> = (0..dom.len())
+                                .map(|i| syn::Index::from(i))
+                                .collect();
+                            let gen = quote! {
+                                impl From<(#(#dom),*)> for #cod {
+                                    fn from(item: (#(#dom),*)) -> Self {
+                                        #cod::#variant_ident(#(item.#idx),*)
+                                    }
+                                }
+                            };
+                            res.extend(TokenStream::from(gen));
                         }
                     }
+                    Fields::Named(field) => {
+                        if field.named.len() == 1 {
+                            let field = field.named.first().expect("EnumGenerator can only be derived for enum variants with one field");
+                            let field_ident = &field.ident;
+
+                            let dom = &field.ty;
+                            let gen = quote! {
+                                impl From<#dom> for #cod {
+                                    fn from(item: #dom) -> Self {
+                                        #cod::#variant_ident { #field_ident: item }
+                                    }
+                                }
+                            };
+                            res.extend(TokenStream::from(gen));
+                        } else {
+                            let dom: Vec<_> = (&field.named)
+                                .into_iter()
+                                .map(|f| &f.ty)
+                                .collect();
+                            let idx: Vec<_> = (0..dom.len())
+                                .map(|i| syn::Index::from(i))
+                                .collect();
+                            let field_idents: Vec<_> = (&field.named)
+                                .into_iter()
+                                .map(|f| &f.ident)
+                                .collect();
+                            let gen = quote! {
+                                impl From<(#(#dom),*)> for #cod {
+                                    fn from(item: (#(#dom),*)) -> Self {
+                                        #cod::#variant_ident { #( #field_idents: item.#idx ),* }
+                                    }
+                                }
+                            };
+                            res.extend(TokenStream::from(gen));
+                        }
+                    }
+                    Fields::Unit => {
+                        let gen = quote! {
+                            impl From<()> for #cod {
+                                fn from(_: ()) -> Self {
+                                    #cod::#variant_ident
+                                }
+                            }
+                        };
+                        res.extend(TokenStream::from(gen));
+                    }
                 };
-                res.extend(TokenStream::from(gen));
             }
         }
         _ => unreachable!("EnumGenerator can only be derived for enums"),
