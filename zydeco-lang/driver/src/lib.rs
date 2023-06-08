324939570b16ea2774293ab3d78131bcb32c4c94
LighghtEeloo <litiaeeloo@gmail.com>
2023-06-09 04:12:14 +0800

    [driver] merge driver to lib

--- zydeco-lang/driver/src/lib.rs
+++ zydeco-lang/driver/src/lib.rs
@@ -4,10 +4,34 @@
 pub mod parsed;
 pub mod resolved;
 pub mod package;
-pub mod driver;
 pub mod err;
 
-pub use driver::Driver;
+use self::{err::SurfaceError, parsed::*, resolved::*};
+use std::path::Path;
+
+#[derive(Default)]
+pub struct Driver {}
+
+impl Driver {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn single_file(&mut self, path: impl AsRef<Path>) -> Result<(), SurfaceError> {
+        let mut deps = DependencyTracker::default();
+
+        // parse
+        let mut parsed = ParsedMap::default();
+        let id = parsed.add_file_parsed(ParsedMap::parse_file(path)?);
+        let std = parsed.add_file_parsed(ParsedMap::std());
+        deps.update_dep(id, std);
+
+        // resolve
+        let mut resolved = ResolvedMap::new(deps);
+        resolved.resolve_one_by_one(&parsed)?;
+
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests;
