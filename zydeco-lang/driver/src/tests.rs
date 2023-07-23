0d8fac3104a7ba7947ff69f1a119dde26d15fedd
akaiDing <tingtind@umich.edu>
2023-07-23 16:53:45 -0400

    fix useless test

--- zydeco-lang/driver/src/tests.rs
+++ zydeco-lang/driver/src/tests.rs
@@ -1,6 +1,6 @@
 use super::*;
 #[test]
-fn driver_1() {
+fn single_file() {
     std::env::set_current_dir("../../").unwrap();
     let mut driver = Driver::default();
     let _ = driver.load_project("zydeco-lang/tests/nonzero-exit-code/interpreter_new.zydeco").map_err(|_e| {
@@ -8,17 +8,17 @@ fn driver_1() {
         panic!()
     });
 }
-#[test]
-fn driver_2() {
-    let mut driver = Driver::default();
-    let _ = driver.load_project("docs/Std_next").map_err(|_e| {
-        eprintln!("{}", _e);
-        panic!()
-    });
-}
+// #[test]
+// fn driver_2() {
+//     let mut driver = Driver::default();
+//     let _ = driver.load_project("docs/Std_next").map_err(|_e| {
+//         eprintln!("{}", _e);
+//         panic!()
+//     });
+// }
 
 #[test]
-fn driver_3() {
+fn whole_project() {
     let mut driver = Driver::default();
     // seperate interpreter into a whole project
     let _ = driver.load_project("zydeco-lang/tests/whole-project/cbpv-interpreter").map_err(|_e| {
