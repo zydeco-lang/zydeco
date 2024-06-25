4353c1427d0d6d95966df0a65b47165fe16cd989
LitiaEeloo <litiaeeloo@gmail.com>
2024-06-24 20:17:25 -0400

    dummy test binary

---
+++ zydeco-lang/driver/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../lib/std/proj.toml")
+        .canonicalize()
+        .unwrap();
+    let proj = zydeco_driver::package::pack::Package::new(dir).unwrap();
+    match proj.run() {
+        | Ok(_) => {}
+        | Err(err) => {
+            eprintln!("{}", err);
+            panic!("Error running project");
+        }
+    }
+}
