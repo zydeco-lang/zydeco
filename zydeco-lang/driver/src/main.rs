6d214e18b7c9b4906dc7a50207a14d01772b3ac9
LitiaEeloo <litiaeeloo@gmail.com>
2024-06-26 14:35:05 -0400

    migrating test cases (WIP)

--- zydeco-lang/driver/src/main.rs
+++ zydeco-lang/driver/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("../lib/std/proj.toml")
+        .join("../lib/exec/proj.toml")
+        // .join("../lib/std/proj.toml")
         .canonicalize()
         .unwrap();
     let proj = zydeco_driver::package::pack::Package::new(dir).unwrap();
