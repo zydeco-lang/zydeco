b9e2dab6b07029304a93db7eb9ede150f92dc860
LitiaEeloo <litiaeeloo@gmail.com>
2024-10-22 16:20:27 -0400

    recover zls

--- editor/zls/src/document_symbols.rs
+++ editor/zls/src/document_symbols.rs
@@ -1,146 +1,277 @@
-// use std::path::PathBuf;
+use async_lsp::lsp_types::{DocumentSymbol, DocumentSymbolResponse, Range, SymbolKind};
+use async_lsp::{ErrorCode, ResponseError};
+use lsp_textdocument::FullTextDocument;
+use zydeco_driver::{check::pack::PackageStew, prelude::b, Package};
+use zydeco_utils::arena::ArcGlobalAlloc;
 
-// use async_lsp::lsp_types::{DocumentSymbol, DocumentSymbolResponse, Range, SymbolKind};
-// use async_lsp::{ErrorCode, ResponseError};
-// use lsp_textdocument::FullTextDocument;
-// use zydeco_lang::surface::parse::syntax::Declaration;
-// use zydeco_lang::syntax::{CtorV, DeclSymbol, DtorV, NameT};
-// use zydeco_lang::utils::span::{Sp, Span, SpanView};
-// use zydeco_lang::zydeco::ZydecoFile;
+use crate::text_position::span_to_range;
 
-// use crate::text_position::span_to_range;
+pub fn handle(
+    document: &FullTextDocument,
+) -> Result<Option<DocumentSymbolResponse>, ResponseError> {
+    let alloc = ArcGlobalAlloc::new();
 
-// pub fn handle(
-//     document: &FullTextDocument,
-// ) -> Result<Option<DocumentSymbolResponse>, ResponseError> {
-//     let ast = ZydecoFile::parse_src(document.get_content(None), PathBuf::new()).map_err(|err| {
-//         ResponseError::new(ErrorCode::REQUEST_FAILED, format!("parsing error: {err}"))
-//     })?;
+    let stew = Package::Repl(document.get_content(None).to_string()).parse_package(alloc).map_err(
+        |err| ResponseError::new(ErrorCode::REQUEST_FAILED, format!("parsing error: {err}")),
+    )?;
 
-//     let symbols = ast
-//         .inner
-//         .declarations
-//         .iter()
-//         .flat_map(|decl| declaration_to_symbol(decl, document))
-//         .collect();
-//     Ok(Some(DocumentSymbolResponse::Nested(symbols)))
-// }
+    let symbols = stew
+        .top
+        .0
+        .iter()
+        .flat_map(|decl| DocumentSymbolContext { document, stew: &stew }.declaration(decl))
+        .collect();
+    Ok(Some(DocumentSymbolResponse::Nested(symbols)))
+}
 
-// fn declaration_to_symbol(
-//     decl: &Sp<DeclSymbol<Declaration>>, document: &FullTextDocument,
-// ) -> Vec<DocumentSymbol> {
-//     match &decl.inner.inner {
-//         | Declaration::Module(module_def) => {
-//             let children = module_def
-//                 .declarations
-//                 .iter()
-//                 .flat_map(|decl| declaration_to_symbol(decl, document))
-//                 .collect();
+pub struct DocumentSymbolContext<'a> {
+    pub document: &'a FullTextDocument,
+    pub stew: &'a PackageStew,
+}
 
-//             match &module_def.name {
-//                 | Some(name_ref) => {
-//                     vec![name_to_symbol(
-//                         name_ref,
-//                         decl.span(),
-//                         document,
-//                         SymbolKind::MODULE,
-//                         children,
-//                     )]
-//                 }
-//                 | None => children,
-//             }
-//         }
-//         | Declaration::UseDef(_) => vec![],
-//         | Declaration::Data(data_def) => {
-//             let children =
-//                 data_def.ctors.iter().map(|ctor| ctorv_to_symbol(&ctor.ctorv, document)).collect();
+impl<'a> DocumentSymbolContext<'a> {
+    fn declaration(&self, id: &b::DeclId) -> Vec<DocumentSymbol> {
+        let b::Modifiers { public: _, external: _, inner } = &self.stew.arena.decls[id];
+        match inner {
+            | b::Declaration::AliasBody(d) => {
+                let b::AliasBody { binder, bindee } = d;
+                let binder = self.pattern(binder);
+                let bindee = self.term(bindee);
+                [].into_iter().chain(binder).chain(bindee).collect()
+            }
+            | b::Declaration::AliasHead(d) => {
+                let b::AliasHead { binder, ty } = d;
+                let binder = self.pattern(binder);
+                let ty = ty.map(|ty| self.term(&ty)).unwrap_or_default();
+                [].into_iter().chain(binder).chain(ty).collect()
+            }
+            | b::Declaration::Module(d) => {
+                let b::Module { name: _, top } = d;
+                let top = top.0.iter().flat_map(|decl| self.declaration(decl)).collect();
+                top
+            }
+            | b::Declaration::Exec(d) => {
+                let b::Exec(term) = d;
+                self.term(term)
+            }
+        }
+    }
+    fn definition(&self, id: &b::DefId) -> DocumentSymbol {
+        let def = &self.stew.arena.defs[id];
+        let tid = self.stew.arena.textual.back(&id.clone().into()).unwrap();
+        let span = &self.stew.spans[tid];
+        let name_range = span_to_range(span, self.document);
+        document_symbol_new(def.0.to_string(), SymbolKind::VARIABLE, name_range, name_range, None)
+    }
+    fn pattern(&self, id: &b::PatId) -> Vec<DocumentSymbol> {
+        let pat = &self.stew.arena.pats[id];
+        let tid = self.stew.arena.textual.back(&id.clone().into()).unwrap();
+        let span = &self.stew.spans[tid];
+        match pat {
+            | b::Pattern::Ann(p) => {
+                let b::Ann { tm, ty } = p;
+                let tm = self.pattern(tm);
+                let ty = self.term(ty);
+                [].into_iter().chain(tm).chain(ty).collect()
+            }
+            | b::Pattern::Hole(p) => {
+                let b::Hole = p;
+                let range = span_to_range(span, self.document);
+                vec![document_symbol_new("_".to_string(), SymbolKind::CONSTANT, range, range, None)]
+            }
+            | b::Pattern::Var(p) => {
+                let def = p;
+                vec![self.definition(def)]
+            }
+            | b::Pattern::Ctor(p) => {
+                let b::Ctor(_ctorv, body) = p;
+                let body = self.pattern(body);
+                [].into_iter().chain(body).collect()
+            }
+            | b::Pattern::Triv(p) => {
+                let b::Triv = p;
+                vec![]
+            }
+            | b::Pattern::Cons(p) => {
+                let b::Cons(a, b) = p;
+                let a = self.pattern(a);
+                let b = self.pattern(b);
+                [].into_iter().chain(a).chain(b).collect()
+            }
+        }
+    }
+    fn term(&self, id: &b::TermId) -> Vec<DocumentSymbol> {
+        let term = &self.stew.arena.terms[id];
+        let tid = self.stew.arena.textual.back(&id.clone().into()).unwrap();
+        let span = &self.stew.spans[tid];
+        match term {
+            | b::Term::Internal(t) => {
+                let _ = t;
+                vec![]
+            }
+            | b::Term::Sealed(t) => {
+                let b::Sealed(body) = t;
+                self.term(body)
+            }
+            | b::Term::Ann(t) => {
+                let b::Ann { tm, ty } = t;
+                let tm = self.term(tm);
+                let ty = self.term(ty);
+                [].into_iter().chain(tm).chain(ty).collect()
+            }
+            | b::Term::Hole(t) => {
+                let b::Hole = t;
+                let range = span_to_range(span, self.document);
+                vec![document_symbol_new("_".to_string(), SymbolKind::CONSTANT, range, range, None)]
+            }
+            | b::Term::Var(t) => {
+                let def = t;
+                let name_range = span_to_range(span, self.document);
+                vec![document_symbol_new(
+                    def.2.to_string(),
+                    SymbolKind::VARIABLE,
+                    name_range,
+                    name_range,
+                    None,
+                )]
+            }
+            | b::Term::Triv(t) => {
+                let b::Triv = t;
+                vec![]
+            }
+            | b::Term::Cons(t) => {
+                let b::Cons(a, b) = t;
+                let a = self.term(a);
+                let b = self.term(b);
+                [].into_iter().chain(a).chain(b).collect()
+            }
+            | b::Term::Abs(t) => {
+                let b::Abs(binder, body) = t;
+                let binder = self.pattern(binder);
+                let body = self.term(body);
+                [].into_iter().chain(binder).chain(body).collect()
+            }
+            | b::Term::App(t) => {
+                let b::App(f, a) = t;
+                let f = self.term(f);
+                let a = self.term(a);
+                [].into_iter().chain(f).chain(a).collect()
+            }
+            | b::Term::Fix(t) => {
+                let b::Fix(binder, body) = t;
+                let binder = self.pattern(binder);
+                let body = self.term(body);
+                [].into_iter().chain(binder).chain(body).collect()
+            }
+            | b::Term::Pi(t) => {
+                let b::Pi(binder, body) = t;
+                let binder = self.pattern(binder);
+                let body = self.term(body);
+                [].into_iter().chain(binder).chain(body).collect()
+            }
+            | b::Term::Sigma(t) => {
+                let b::Sigma(binder, body) = t;
+                let binder = self.pattern(binder);
+                let body = self.term(body);
+                [].into_iter().chain(binder).chain(body).collect()
+            }
+            | b::Term::Thunk(t) => {
+                let b::Thunk(body) = t;
+                self.term(body)
+            }
+            | b::Term::Force(t) => {
+                let b::Force(body) = t;
+                self.term(body)
+            }
+            | b::Term::Ret(t) => {
+                let b::Ret(body) = t;
+                self.term(body)
+            }
+            | b::Term::Do(t) => {
+                let b::Bind { binder, bindee, tail } = t;
+                let binder = self.pattern(binder);
+                let bindee = self.term(bindee);
+                let tail = self.term(tail);
+                [].into_iter().chain(binder).chain(bindee).chain(tail).collect()
+            }
+            | b::Term::Let(t) => {
+                let b::PureBind { binder, bindee, tail } = t;
+                let binder = self.pattern(binder);
+                let bindee = self.term(bindee);
+                let tail = self.term(tail);
+                [].into_iter().chain(binder).chain(bindee).chain(tail).collect()
+            }
+            | b::Term::Data(t) => {
+                let b::Data { arms } = t;
+                arms.iter().flat_map(|arm| self.term(&arm.param)).collect()
+            }
+            | b::Term::CoData(t) => {
+                let b::CoData { arms } = t;
+                arms.iter().flat_map(|arm| self.term(&arm.out)).collect()
+            }
+            | b::Term::Ctor(t) => {
+                let b::Ctor(_ctorv, body) = t;
+                let body = self.term(body);
+                [].into_iter().chain(body).collect()
+            }
+            | b::Term::Match(t) => {
+                let b::Match { scrut, arms } = t;
+                let scrut = self.term(scrut);
+                let arms: Vec<_> = arms
+                    .iter()
+                    .flat_map(|arm| {
+                        [].into_iter().chain(self.pattern(&arm.binder)).chain(self.term(&arm.tail))
+                    })
+                    .collect();
+                [].into_iter().chain(scrut).chain(arms).collect()
+            }
+            | b::Term::CoMatch(t) => {
+                let b::CoMatch { arms } = t;
+                arms.iter().flat_map(|arm| [].into_iter().chain(self.term(&arm.tail))).collect()
+            }
+            | b::Term::Dtor(t) => {
+                let b::Dtor(body, _dtorv) = t;
+                let body = self.term(body);
+                [].into_iter().chain(body).collect()
+            }
+            | b::Term::WithBlock(t) => {
+                let _ = t;
+                vec![]
+            }
+            | b::Term::MBlock(t) => {
+                let b::MBlock { mo, body } = t;
+                let mo = self.term(mo);
+                let body = self.term(body);
+                [].into_iter().chain(mo).chain(body).collect()
+            }
+            | b::Term::WBlock(t) => {
+                let _ = t;
+                vec![]
+            }
+            | b::Term::Lit(t) => {
+                let _ = t;
+                vec![]
+            }
+        }
+    }
+}
 
-//             vec![name_to_symbol(&data_def.name, decl.span(), document, SymbolKind::ENUM, children)]
-//         }
-//         | Declaration::Codata(codata_def) => {
-//             let children = codata_def
-//                 .dtors
-//                 .iter()
-//                 .map(|dtor| dtorv_to_symbol(&dtor.dtorv, document))
-//                 .collect();
-
-//             vec![name_to_symbol(
-//                 &codata_def.name,
-//                 decl.span(),
-//                 document,
-//                 SymbolKind::CLASS,
-//                 children,
-//             )]
-//         }
-//         | Declaration::Alias(alias_def) => {
-//             // TODO: lookup symbol to display correct kind
-//             vec![name_to_symbol(&alias_def.name, decl.span(), document, SymbolKind::ENUM, vec![])]
-//         }
-//         | Declaration::Define(def) => {
-//             // TODO: handle local definitions
-//             let children = vec![];
-
-//             let kind = match def.0.params.len() {
-//                 | 0 => SymbolKind::CONSTANT,
-//                 | _ => SymbolKind::FUNCTION,
-//             };
-
-//             vec![name_to_symbol(&def.0.name.0, decl.span(), document, kind, children)]
-//         }
-//         // TODO: handle main, local definitions
-//         | Declaration::Main(_) => vec![],
-//     }
-// }
-
-// fn name_to_symbol(
-//     name: &impl NameT, item_span: &Span, document: &FullTextDocument, kind: SymbolKind,
-//     children: Vec<DocumentSymbol>,
-// ) -> DocumentSymbol {
-//     let name_view = name.name();
-//     let name_str = name_view.ident.inner.clone();
-//     let name_range = span_to_range(name_view.ident.span(), document);
-
-//     let item_range = span_to_range(item_span, document);
-
-//     document_symbol_new(name_str, kind, item_range, name_range, Some(children))
-// }
-
-// fn ctorv_to_symbol(ctorv: &CtorV, document: &FullTextDocument) -> DocumentSymbol {
-//     let name_range = span_to_range(ctorv.span(), document);
-
-//     // TODO: get span of entire constructor
-//     document_symbol_new(
-//         ctorv.name().to_owned(),
-//         SymbolKind::CONSTRUCTOR,
-//         name_range,
-//         name_range,
-//         None,
-//     )
-// }
-
-// fn dtorv_to_symbol(dtorv: &DtorV, document: &FullTextDocument) -> DocumentSymbol {
-//     let name_range = span_to_range(dtorv.span(), document);
-
-//     // TODO: get span of entire destructor
-//     document_symbol_new(dtorv.name().to_owned(), SymbolKind::METHOD, name_range, name_range, None)
-// }
-
-// /// Constructs a [`DocumentSymbol`]. Since `DocumentSymbol::deprecated` is marked as
-// /// `#[deprecated]`, we must put `#[allow(deprecated)]` on any function directly constructing a
-// /// `DocumentSymbol`. We define this function to minimize where this happens.
-// #[allow(deprecated)]
-// fn document_symbol_new(
-//     name: String, kind: SymbolKind, range: Range, selection_range: Range,
-//     children: Option<Vec<DocumentSymbol>>,
-// ) -> DocumentSymbol {
-//     DocumentSymbol {
-//         name,
-//         detail: None,
-//         kind,
-//         tags: None,
-//         deprecated: None,
-//         range,
-//         selection_range,
-//         children,
-//     }
-// }
+/// Constructs a [`DocumentSymbol`]. Since `DocumentSymbol::deprecated` is marked as
+/// `#[deprecated]`, we must put `#[allow(deprecated)]` on any function directly constructing a
+/// `DocumentSymbol`. We define this function to minimize where this happens.
+#[allow(deprecated)]
+fn document_symbol_new(
+    name: String, kind: SymbolKind, range: Range, selection_range: Range,
+    children: Option<Vec<DocumentSymbol>>,
+) -> DocumentSymbol {
+    DocumentSymbol {
+        name,
+        detail: None,
+        kind,
+        tags: None,
+        deprecated: None,
+        range,
+        selection_range,
+        children,
+    }
+}
--- editor/zls/src/documents.rs
+++ editor/zls/src/documents.rs
@@ -1,89 +1,89 @@
-// use std::collections::HashMap;
-// use std::future::Future;
-// use std::ops::Deref;
-// use std::sync::Arc;
+use std::collections::HashMap;
+use std::future::Future;
+use std::ops::Deref;
+use std::sync::Arc;
 
-// use async_lsp::lsp_types::{
-//     DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, Url,
-// };
-// use async_lsp::{ErrorCode, ResponseError};
-// use lsp_textdocument::FullTextDocument;
-// use tokio::sync::{OwnedRwLockReadGuard, RwLock};
+use async_lsp::lsp_types::{
+    DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, Url,
+};
+use async_lsp::{ErrorCode, ResponseError};
+use lsp_textdocument::FullTextDocument;
+use tokio::sync::{OwnedRwLockReadGuard, RwLock};
 
-// // TODO: virtual filesystem abstraction to handle on-disk and client-provided documents
-// #[derive(Default)]
-// pub struct DocumentStore {
-//     inner: Arc<RwLock<HashMap<Url, FullTextDocument>>>,
-// }
+// TODO: virtual filesystem abstraction to handle on-disk and client-provided documents
+#[derive(Default)]
+pub struct DocumentStore {
+    inner: Arc<RwLock<HashMap<Url, FullTextDocument>>>,
+}
 
-// impl DocumentStore {
-//     /// Returns a [`Future`] which, if the document at the requested [`Url`] exists, resolves to an
-//     /// owned read lock providing access to that document.
-//     pub fn get_document(
-//         &self, url: Url,
-//     ) -> impl Future<Output = Result<impl Deref<Target = FullTextDocument>, ResponseError>>
-//            + Send
-//            + 'static {
-//         // Notes on the signature and implementation:
-//         // - The `Future`s returned by the LSP router require `Send + 'static`, so for convenience,
-//         //   we want to provide a fully owned, thread safe `Future` given a reference to the
-//         //   document store.
-//         // - For correctness reasons, it is nice to put an `RwLock` around everything, since this
-//         //   prevents modifications to the project from impacting ongoing analysis. A more complex,
-//         //   but potentially faster, approach would be to allow for limited forms of this, or to
-//         //   track and protect dependencies/dependents so that fined-grained locking can be used
-//         //   instead.
-//         // - The `impl Deref<...>` type name is used because the actual type is an implementation
-//         //   detail (depends on our `RwLock` implementation) and is long and complex.
+impl DocumentStore {
+    /// Returns a [`Future`] which, if the document at the requested [`Url`] exists, resolves to an
+    /// owned read lock providing access to that document.
+    pub fn get_document(
+        &self, url: Url,
+    ) -> impl Future<Output = Result<impl Deref<Target = FullTextDocument>, ResponseError>>
+           + Send
+           + 'static {
+        // Notes on the signature and implementation:
+        // - The `Future`s returned by the LSP router require `Send + 'static`, so for convenience,
+        //   we want to provide a fully owned, thread safe `Future` given a reference to the
+        //   document store.
+        // - For correctness reasons, it is nice to put an `RwLock` around everything, since this
+        //   prevents modifications to the project from impacting ongoing analysis. A more complex,
+        //   but potentially faster, approach would be to allow for limited forms of this, or to
+        //   track and protect dependencies/dependents so that fined-grained locking can be used
+        //   instead.
+        // - The `impl Deref<...>` type name is used because the actual type is an implementation
+        //   detail (depends on our `RwLock` implementation) and is long and complex.
 
-//         let inner = Arc::clone(&self.inner);
-//         async move {
-//             let guard = inner.read_owned().await;
-//             let doc = OwnedRwLockReadGuard::try_map(guard, |doc_map| doc_map.get(&url)).ok();
-//             doc.ok_or_else(|| {
-//                 ResponseError::new(
-//                     ErrorCode::INVALID_PARAMS,
-//                     format!("could not find document {url}"),
-//                 )
-//             })
-//         }
-//     }
+        let inner = Arc::clone(&self.inner);
+        async move {
+            let guard = inner.read_owned().await;
+            let doc = OwnedRwLockReadGuard::try_map(guard, |doc_map| doc_map.get(&url)).ok();
+            doc.ok_or_else(|| {
+                ResponseError::new(
+                    ErrorCode::INVALID_PARAMS,
+                    format!("could not find document {url}"),
+                )
+            })
+        }
+    }
 
-//     pub fn open_document(&self, params: DidOpenTextDocumentParams) -> impl Future<Output = ()> {
-//         let inner = Arc::clone(&self.inner);
-//         async move {
-//             let doc = FullTextDocument::new(
-//                 params.text_document.language_id,
-//                 params.text_document.version,
-//                 params.text_document.text,
-//             );
+    pub fn open_document(&self, params: DidOpenTextDocumentParams) -> impl Future<Output = ()> {
+        let inner = Arc::clone(&self.inner);
+        async move {
+            let doc = FullTextDocument::new(
+                params.text_document.language_id,
+                params.text_document.version,
+                params.text_document.text,
+            );
 
-//             let mut doc_map = inner.write().await;
-//             doc_map.insert(params.text_document.uri, doc);
-//         }
-//     }
+            let mut doc_map = inner.write().await;
+            doc_map.insert(params.text_document.uri, doc);
+        }
+    }
 
-//     pub fn change_document(&self, params: DidChangeTextDocumentParams) -> impl Future<Output = ()> {
-//         let inner = Arc::clone(&self.inner);
-//         async move {
-//             let mut doc_map = inner.write().await;
-//             let Some(doc) = doc_map.get_mut(&params.text_document.uri) else {
-//                 return;
-//             };
+    pub fn change_document(&self, params: DidChangeTextDocumentParams) -> impl Future<Output = ()> {
+        let inner = Arc::clone(&self.inner);
+        async move {
+            let mut doc_map = inner.write().await;
+            let Some(doc) = doc_map.get_mut(&params.text_document.uri) else {
+                return;
+            };
 
-//             // hack around mismatched `lsp_types` version
-//             let changes: Vec<_> =
-//                 serde_json::from_value(serde_json::to_value(params.content_changes).unwrap())
-//                     .unwrap();
-//             doc.update(&changes, params.text_document.version);
-//         }
-//     }
+            // hack around mismatched `lsp_types` version
+            let changes: Vec<_> =
+                serde_json::from_value(serde_json::to_value(params.content_changes).unwrap())
+                    .unwrap();
+            doc.update(&changes, params.text_document.version);
+        }
+    }
 
-//     pub fn close_document(&self, params: DidCloseTextDocumentParams) -> impl Future<Output = ()> {
-//         let inner = Arc::clone(&self.inner);
-//         async move {
-//             let mut doc_map = inner.write().await;
-//             doc_map.remove(&params.text_document.uri);
-//         }
-//     }
-// }
+    pub fn close_document(&self, params: DidCloseTextDocumentParams) -> impl Future<Output = ()> {
+        let inner = Arc::clone(&self.inner);
+        async move {
+            let mut doc_map = inner.write().await;
+            doc_map.remove(&params.text_document.uri);
+        }
+    }
+}
