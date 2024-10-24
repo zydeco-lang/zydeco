964b2c9d78ca4dd75a3e7f0bdb00cf3ad590d065
Yuchen <lighght@umich.edu>
2024-10-23 20:50:52 -0400

    add spell tests

--- lang/driver/src/tests.rs
+++ lang/driver/src/tests.rs
@@ -91,6 +91,18 @@ mod icfp {
     lib_proj_bin!(icfp, transformers, "transformers");
 }
 
+mod spell {
+    lib_proj_bin!(spell, toplevel, "0-toplevel");
+    lib_proj_bin!(spell, thunk_ret, "1-thunk-ret");
+    lib_proj_bin!(spell, data_codata, "2-data-codata");
+    lib_proj_bin!(spell, object, "3-object");
+    lib_proj_bin!(spell, y_combinator, "4-y-combinator");
+    lib_proj_bin!(spell, mutual_rec, "5-mutual-rec");
+    lib_proj_bin!(spell, cps, "6-cps");
+    // lib_proj_bin!(spell, call_by_need, "7-call-by-need");
+    lib_proj_bin!(spell, lense_prism_optics, "8-lense-prism-optics");
+}
+
 mod monadic {
     lib_proj_bin!(monadic, quote, "quote");
     lib_proj_bin!(monadic, trans, "trans");
