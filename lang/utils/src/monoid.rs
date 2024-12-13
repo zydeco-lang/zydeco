5e8e1a8484c31977a3cb46f72014a50bbf6b773b
LitiaEeloo <litiaeeloo@gmail.com>
2024-12-13 00:31:52 -0500

    incr: ctx and coctx folds on scoped

--- lang/utils/src/monoid.rs
+++ lang/utils/src/monoid.rs
@@ -1,21 +1,10 @@
-use super::span::{Sp, Span};
+use std::ops::Add;
 
-pub trait Monoid: Sized {
-    fn empty() -> Self;
-    fn append(self, other: Self) -> Self;
+pub trait Monoid: Default + Add<Output = Self> + Sized {
     fn extend(self, others: impl Iterator<Item = Self>) -> Self {
-        others.fold(self, Self::append)
+        others.fold(self, Self::add)
     }
     fn concat(others: impl Iterator<Item = Self>) -> Self {
-        Self::empty().extend(others)
-    }
-}
-
-impl<T: Monoid> Monoid for Sp<T> {
-    fn empty() -> Self {
-        Span::dummy().make(T::empty())
-    }
-    fn append(self, other: Self) -> Self {
-        other.info.make(self.inner.append(other.inner))
+        Self::default().extend(others)
     }
 }
