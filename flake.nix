11a4f650c8082fa2bb1a05341940206774b256a8
LitiaEeloo <litiaeeloo@gmail.com>
2025-06-13 16:13:07 -0400

    feat: add nix configuration

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Rust 2024 project with pinned toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Fenix gives pre-built official toolchains
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { self, nixpkgs, fenix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs   = import nixpkgs { inherit system; };
+      rust   = fenix.packages.${system}.stable.toolchain;   # Rust 1.85 (Edition 2024)
+    in {
+      packages.${system}.zydeco =
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "zydeco";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          # Edition 2024 needs no extra flags; cargo picks it up from Cargo.toml
+        };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          rust
+          pkgs.cargo-nextest
+          pkgs.rust-analyzer
+          pkgs.clippy
+          pkgs.pkg-config pkgs.openssl
+        ];
+        RUST_BACKTRACE = "1";
+      };
+
+      apps.${system}.zydeco = {
+        type = "app";
+        program = "${self.packages.${system}.zydeco}/bin/zydeco";
+      };
+    };
+}
