d5e98f430fd87461633356bcbc145b93c4a0f9fe
LitiaEeloo <litiaeeloo@gmail.com>
2025-12-05 02:08:06 -0500

    repo: init stack-passing-style rewrite

---
+++ lang/stack/src/lib.rs
@@ -0,0 +1,7 @@
+//! This crate implements the Zydeco-Intermediate-Representation (ZIR),
+//! a high-level IR abstraction in stack-passing style.
+
+pub mod syntax;
+// pub mod arena;
+// pub mod fmt;
+// pub mod lower;
