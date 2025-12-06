c65e5d122758dd2d477d94c110cd874526fdc4ab
LitiaEeloo <litiaeeloo@gmail.com>
2025-12-06 02:27:55 -0500

    incr: refactor cli commands

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use std::{fs::File, io::Write, path::PathBuf, process::Command};
 // use zydeco_cli::{Cli, Commands, Repl};
 use zydeco_cli::{Cli, Commands};
-use zydeco_driver::{BuildSystem, ProgKont};
+use zydeco_driver::{BuildSystem, ProgKont, PackId};
 
 fn main() -> Result<(), ()> {
     env_logger::init();
@@ -36,12 +36,11 @@ fn main() -> Result<(), ()> {
     }
 }
 
-fn run_files(
-    paths: Vec<PathBuf>, bin: Option<String>, dry: bool, verbose: bool, args: Vec<String>,
-) -> Result<i32, String> {
+fn setup_build_system(paths: Vec<PathBuf>, bin: Option<String>) -> Result<(BuildSystem, PackId), String> {
     let mut build_sys = BuildSystem::new();
     let mut packs = Vec::new();
     let mut files = Vec::new();
+    
     for path in paths {
         // for dir, try finding "proj.toml" under it
         if path.is_dir() {
@@ -65,6 +64,7 @@ fn run_files(
             }
         }
     }
+    
     if files.is_empty() {
         for pack in packs {
             build_sys.add_binary_in_package(pack).map_err(|e| e.to_string())?;
@@ -75,7 +75,15 @@ fn run_files(
             build_sys.mark(pack).map_err(|e| e.to_string())?;
         }
     }
+    
     let pack = build_sys.pick_marked(bin).map_err(|e| e.to_string())?;
+    Ok((build_sys, pack))
+}
+
+fn run_files(
+    paths: Vec<PathBuf>, bin: Option<String>, dry: bool, verbose: bool, args: Vec<String>,
+) -> Result<i32, String> {
+    let (build_sys, pack) = setup_build_system(paths, bin)?;
     match build_sys.run_pack(pack, &args, dry, verbose).map_err(|e| e.to_string())? {
         | ProgKont::Dry => Ok(0),
         | ProgKont::Ret(_) => unreachable!(),
@@ -87,43 +95,7 @@ fn build_files(
     paths: Vec<PathBuf>, bin: Option<String>, target: String, build_dir: PathBuf, stub: PathBuf,
     link_existing: bool, execute: bool, _dry: bool, verbose: bool,
 ) -> Result<i32, String> {
-    let mut build_sys = BuildSystem::new();
-    let mut packs = Vec::new();
-    let mut files = Vec::new();
-    for path in paths {
-        // for dir, try finding "proj.toml" under it
-        if path.is_dir() {
-            let proj = path.join("proj.toml");
-            if proj.exists() {
-                let pack = build_sys.add_local_package(proj).map_err(|e| e.to_string())?;
-                packs.push(pack);
-                continue;
-            }
-            // fallback to adding the dir itself
-        }
-        match path.extension() {
-            | Some(ext) if ext == "toml" => {
-                // package
-                let pack = build_sys.add_local_package(path).map_err(|e| e.to_string())?;
-                packs.push(pack);
-            }
-            | Some(_) | None => {
-                // single file
-                files.push(path);
-            }
-        }
-    }
-    if files.is_empty() {
-        for pack in packs {
-            build_sys.add_binary_in_package(pack).map_err(|e| e.to_string())?;
-        }
-    } else {
-        for file in files.iter() {
-            let pack = build_sys.add_orphan_file(file).map_err(|e| e.to_string())?;
-            build_sys.mark(pack).map_err(|e| e.to_string())?;
-        }
-    }
-    let pack = build_sys.pick_marked(bin).map_err(|e| e.to_string())?;
+    let (build_sys, pack) = setup_build_system(paths, bin)?;
 
     match target.as_str() {
         | "zir" => {
