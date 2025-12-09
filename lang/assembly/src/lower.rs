9096e2c88aeb9704583d14ba520715aa02cece66
Yuchen Jiang <lighght@umich.edu>
2025-12-08 19:39:44 -0500

    sisy: bootstrap lowerer in assembly

--- lang/assembly/src/lower.rs
+++ lang/assembly/src/lower.rs
@@ -1,168 +1,170 @@
-// //! Lower from [`zydeco_stack::StackArena`] to [`AssemblyArena`].
-// //!
-// //! - All global variables and all values are
-// //!   compiled into programs that pushes the value onto the stack.
-// //! - All computations and stacks are compiled into programs.
+//! Lower from [`zydeco_stack::StackArena`] to [`AssemblyArena`].
+//!
+//! - All global variables and all values are
+//!   compiled into programs that pushes the value onto the stack.
+//! - All computations and stacks are compiled into programs.
 
-// use super::arena::{AssemblyArena, AssemblyArenaLike};
-// use super::syntax::*;
-// use zydeco_stack::arena::StackArena;
-// use zydeco_stack::syntax as sk;
-// use zydeco_statics::tyck::arena::StaticsArena;
-// use zydeco_surface::{scoped::arena::ScopedArena, textual::syntax as t};
-// use zydeco_utils::arena::ArcGlobalAlloc;
+use super::arena::{AssemblyArena, AssemblyArenaLike};
+use super::syntax::*;
+use zydeco_stack::arena::StackArena;
+use zydeco_stack::syntax as sk;
+use zydeco_statics::tyck::arena::StaticsArena;
+use zydeco_statics::tyck::syntax as ss;
+use zydeco_surface::{scoped::arena::ScopedArena, textual::arena::SpanArena};
+use zydeco_utils::arena::ArcGlobalAlloc;
 
-// pub trait Lower {
-//     type Kont;
-//     type Out;
-//     fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out;
-// }
+pub trait Lower {
+    type Kont;
+    type Out;
+    fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out;
+}
 
-// pub struct Lowerer<'a> {
-//     pub arena: AssemblyArena,
-//     pub spans: &'a t::SpanArena,
-//     pub scoped: &'a ScopedArena,
-//     pub statics: &'a StaticsArena,
-//     pub stack: &'a StackArena,
-// }
+pub struct Lowerer<'a> {
+    pub arena: AssemblyArena,
+    pub spans: &'a SpanArena,
+    pub scoped: &'a ScopedArena,
+    pub statics: &'a StaticsArena,
+    pub stack: &'a StackArena,
+}
 
-// impl<'a> Lowerer<'a> {
-//     pub fn new(
-//         alloc: ArcGlobalAlloc, spans: &'a t::SpanArena, scoped: &'a ScopedArena,
-//         statics: &'a StaticsArena, stack: &'a StackArena,
-//     ) -> Self {
-//         let arena = AssemblyArena::new(alloc);
-//         Self { arena, spans, scoped, statics, stack }
-//     }
+impl<'a> Lowerer<'a> {
+    pub fn new(
+        alloc: ArcGlobalAlloc, spans: &'a SpanArena, scoped: &'a ScopedArena,
+        statics: &'a StaticsArena, stack: &'a StackArena,
+    ) -> Self {
+        let arena = AssemblyArena::new(alloc);
+        Self { arena, spans, scoped, statics, stack }
+    }
 
-//     pub fn run(mut self) -> AssemblyArena {
-//         // Compile all globals - each global value is compiled into a program that pushes it onto the stack
-//         for (def_id, global) in &self.stack.globals {
-//             match global {
-//                 | sk::Global::Defined(value_id) => {
-//                     // Compile the global value into a program that pushes it onto the stack
-//                     let name = self.scoped.defs[def_id].plain().to_string();
-//                     let label = Label(name.clone());
-//                     // Lower the value - it will create instructions that push the value onto the stack
-//                     // The continuation receives the atom representing the value and creates a Return
-//                     let prog_id = value_id.lower(
-//                         &mut self,
-//                         Box::new(move |atom, lo| {
-//                             // The value is already pushed onto the stack by value.lower's instructions
-//                             // Return the value (the atom represents what's on the stack)
-//                             lo.arena.prog_anon(Program::Return(Return(atom)))
-//                         }),
-//                     );
-//                     // Label the program with the global name
-//                     self.arena.labels.insert(prog_id, label);
-//                 }
-//                 | sk::Global::Extern(_) => {
-//                     // External globals don't need to be compiled - they're referenced when used
-//                 }
-//             }
-//         }
-//         // Lower entry points from StackArena to AssemblyArena
-//         for (compu_id, ()) in &self.stack.entry {
-//             let prog_id = compu_id.lower(&mut self, ());
-//             self.arena.entry.insert(prog_id, ());
-//         }
-//         self.arena
-//     }
-// }
+    pub fn run(mut self) -> AssemblyArena {
+        // Lower entry points from StackArena to AssemblyArena
+        for (compu_id, ()) in &self.stack.entry {
+            // For each entry point, we initialize the globals and then lower the computation.
+            // All globals are statically known, so compile them as symbols.
+            let seq = self.stack.global_seq.clone();
+            let res = seq.into_iter().rev().fold(todo!(), |acc, def_id| todo!());
+            self.arena.entry.insert(res, ());
+        }
+        self.arena
+    }
 
-// impl AsMut<AssemblyArena> for Lowerer<'_> {
-//     fn as_mut(&mut self) -> &mut AssemblyArena {
-//         &mut self.arena
-//     }
-// }
+    /// Find the constructor tag index in a data type, given a value
+    fn find_ctor_tag_idx(
+        &self, value_id: sk::ValueId, ctor_name: &zydeco_syntax::CtorName,
+    ) -> usize {
+        // Get the corresponding statics value
+        let ss::TermId::Value(value) = self
+            .stack
+            .terms
+            .back(&sk::TermId::Value(value_id))
+            .expect("Constructor value not found")
+        else {
+            unreachable!("Constructor is not a value in statics")
+        };
+        // Get the type annotation
+        let ty = self.statics.annotations_value[&value].clone();
+        // Get the data type
+        let ss::Fillable::Done(ss::Type::Data(data)) = self.statics.types[&ty].clone() else {
+            unreachable!("Constructor type is not a data type in statics")
+        };
+        // Find the index of the constructor tag
+        self.statics.datas[&data]
+            .iter()
+            .position(|(tag_branch, _ty)| tag_branch == ctor_name)
+            .expect("Constructor tag not found")
+    }
 
-// impl Lower for sk::VPatId {
-//     type Kont = Box<dyn FnOnce(&mut Lowerer) -> ProgId>;
-//     type Out = ProgId;
+    /// Get the codata type from a computation's type annotation
+    fn find_dtor_tag_idx(&self, compu: ss::CompuId, dtor_name: &zydeco_syntax::DtorName) -> usize {
+        // Get the type annotation
+        let ty = self.statics.annotations_compu[&compu].clone();
+        // Get the codata type
+        let ss::Fillable::Done(ss::Type::CoData(codata)) = self.statics.types[&ty].clone() else {
+            unreachable!("Computation type is not a codata type in statics")
+        };
+        // Find the index of the destructor tag
+        self.statics.codatas[&codata]
+            .iter()
+            .position(|(dtor_branch, _ty)| dtor_branch == dtor_name)
+            .expect("Destructor tag not found")
+    }
 
-//     fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
-//         let vpat = lo.statics.vpats[self].clone();
-//         use zydeco_statics::tyck::syntax::ValuePattern as VPat;
-//         match vpat {
-//             | VPat::Hole(Hole) => todo!(),
-//             | VPat::Var(def) => todo!(),
-//             | VPat::Ctor(Ctor(_ctor, _body)) => todo!(),
-//             | VPat::Triv(Triv) => todo!(),
-//             | VPat::VCons(Cons(a, b)) => todo!(),
-//             | VPat::TCons(Cons(_ty, inner)) => todo!(),
-//         }
-//     }
-// }
+    /// Find the destructor tag index in a codata type, given a computation
+    fn find_dtor_tag_idx_from_compu(
+        &self, compu_id: sk::CompuId, dtor_name: &zydeco_syntax::DtorName,
+    ) -> usize {
+        // Get the corresponding statics computation
+        let ss::TermId::Compu(compu) =
+            self.stack.terms.back(&sk::TermId::Compu(compu_id)).expect("Computation not found")
+        else {
+            unreachable!("Computation is not a computation in statics")
+        };
+        self.find_dtor_tag_idx(*compu, dtor_name)
+    }
 
-// impl Lower for sk::ValueId {
-//     type Kont = Box<dyn FnOnce(Atom, &mut Lowerer) -> ProgId>;
-//     type Out = ProgId;
+    /// Find the destructor tag index in a codata type, given a stack
+    /// Note: This requires the computation context that uses the stack
+    fn find_dtor_tag_idx_from_stack(
+        &self, stack_id: sk::StackId, dtor_name: &zydeco_syntax::DtorName,
+    ) -> usize {
+        // Get the corresponding statics stack
+        let ss::TermId::Compu(compu) =
+            self.stack.terms.back(&sk::TermId::Stack(stack_id)).expect("Stack not found")
+        else {
+            unreachable!("Stack is not a computation in statics")
+        };
+        self.find_dtor_tag_idx(*compu, dtor_name)
+    }
+}
 
-//     fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
-//         let value = lo.stack.values[self].clone();
-//         use sk::Value;
-//         match value {
-//             | Value::Hole(Hole) => todo!(),
-//             | Value::Var(def) => match lo.stack.globals.get(&def) {
-//                 | Some(sk::Global::Extern(_)) => todo!(),
-//                 | Some(sk::Global::Defined(value_id)) => todo!(),
-//                 | None => todo!(),
-//             },
-//             | Value::Clo(sk::Clo { capture: _, stack: _, body }) => todo!(),
-//             | Value::Ctor(Ctor(ctor, body)) => todo!(),
-//             | Value::Triv(Triv) => todo!(),
-//             | Value::VCons(Cons(a, b)) => todo!(),
-//             | Value::Lit(lit) => todo!(),
-//         }
-//     }
-// }
+impl AsMut<AssemblyArena> for Lowerer<'_> {
+    fn as_mut(&mut self) -> &mut AssemblyArena {
+        &mut self.arena
+    }
+}
 
-// impl Lower for sk::StackId {
-//     /// Stacks in ZIR are compiled to instructions
-//     type Kont = Box<dyn FnOnce(&mut Lowerer) -> ProgId>;
-//     type Out = ProgId;
+impl Lower for sk::VPatId {
+    type Kont = Box<dyn FnOnce(VarId, &mut Lowerer) -> ProgId>;
+    type Out = ProgId;
 
-//     fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
-//         let stack = lo.stack.stacks[self].clone();
-//         use sk::Stack;
-//         match stack {
-//             | Stack::Kont(sk::Kont { binder, body }) => {
-//                 // compile the kontinuation into a labelled program,
-//                 // and then push the label and pack the context onto the stack
-//                 todo!()
-//             }
-//             | Stack::Var(sk::Bullet) => kont(lo),
-//             | Stack::Arg(Cons(value, tail)) => {
-//                 // compile the tail, then push the value onto the stack
-//                 tail.lower(
-//                     lo,
-//                     Box::new(move |lo| {
-//                         value.lower(lo, Box::new(move |arg, lo| lo.instr(Push(arg), kont)))
-//                     }),
-//                 )
-//             }
-//             | Stack::Tag(Cons(dtor, tail)) => todo!(),
-//         }
-//     }
-// }
+    fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
+        let vpat = lo.stack.vpats[self].clone();
+        use sk::ValuePattern as VPat;
+        todo!()
+    }
+}
 
-// impl Lower for sk::CompuId {
-//     type Kont = ();
-//     type Out = ProgId;
+/// Values are compiled into programs that push the value onto the stack.
+impl Lower for sk::ValueId {
+    type Kont = Box<dyn FnOnce(&mut Lowerer) -> ProgId>;
+    type Out = ProgId;
 
-//     fn lower(&self, lo: &mut Lowerer, _kont: Self::Kont) -> Self::Out {
-//         let compu = lo.stack.compus[self].clone();
-//         use sk::Computation as Compu;
-//         match compu {
-//             | Compu::Hole(Hole) => todo!(),
-//             | Compu::Fix(Fix(param, body)) => todo!(),
-//             | Compu::Force(sk::SForce { thunk, stack }) => todo!(),
-//             | Compu::Ret(sk::SReturn { stack, value }) => todo!(),
-//             | Compu::Case(Match { scrut, arms }) => todo!(),
-//             | Compu::LetValue(Let { binder, bindee, tail }) => todo!(),
-//             | Compu::LetStack(Let { binder: _, bindee, tail }) => todo!(),
-//             | Compu::LetArg(Let { binder: Cons(vpat, _bullet), bindee, tail }) => todo!(),
-//             | Compu::CoCase(CoMatch { arms }) => todo!(),
-//         }
-//     }
-// }
+    fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
+        let value = lo.stack.values[self].clone();
+        use sk::Value;
+        todo!()
+    }
+}
+
+impl Lower for sk::StackId {
+    /// Stacks in ZIR are compiled to instructions
+    type Kont = Box<dyn FnOnce(&mut Lowerer) -> ProgId>;
+    type Out = ProgId;
+
+    fn lower(&self, lo: &mut Lowerer, kont: Self::Kont) -> Self::Out {
+        let stack = lo.stack.stacks[self].clone();
+        use sk::Stack;
+        todo!()
+    }
+}
+
+impl Lower for sk::CompuId {
+    type Kont = ();
+    type Out = ProgId;
+
+    fn lower(&self, lo: &mut Lowerer, _kont: Self::Kont) -> Self::Out {
+        let compu = lo.stack.compus[self].clone();
+        use sk::Computation as Compu;
+        todo!()
+    }
+}
