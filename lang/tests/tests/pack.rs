09bafa6230c6c0544f61e652a1ac2a8ff196baaa
LitiaEeloo <litiaeeloo@gmail.com>
2026-07-30 06:36:46 -0400

    test: add reusable package integration cases

---
+++ lang/tests/tests/pack.rs
@@ -0,0 +1,6 @@
+use zydeco_tests::interp_proj_bin;
+
+interp_proj_bin!(tests / pack, manifest, "manifest");
+interp_proj_bin!(tests / pack, interleaved, "interleaved");
+interp_proj_bin!(tests / pack, named, "named");
+interp_proj_bin!(tests / pack, monadic, "monadic");
